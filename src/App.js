00ff64310868e6425dc76d6dc511c84f88626a36
bayoticstudios <abdullahishobaloju@gmail.com>
2023-04-06 15:09:40 +0100

    public_key updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
 
 function App() {
   const config = {
-    public_key: 'FLWPUBK_TEST-6d2ab036d6e8a5da7d719f4bf529bdaa-X',
+    public_key: 'FLWPUBK-42258c577ef7b18a1067955f4dff9500-X',
     tx_ref: Date.now(),
     amount: 100,
     currency: 'NGN',
@@ -16,7 +16,7 @@ function App() {
       name: 'john doe',
     },
     customizations: {
-      title: 'My store',
+      title: 'Bayotics Studios',
       description: 'Payment for items in cart',
       logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
     },
